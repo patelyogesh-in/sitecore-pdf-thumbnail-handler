4d84c0ffad06f61d24fca0738604f949342cb030
Yogesh Patel <contact@patelyogesh.in>
2013-11-17 15:36:44 +0530

    PDF Thumbnail Handler for sitecore

---
+++ SitecoreTactics.ThumbnailManager/MediaConverter.cs
@@ -0,0 +1,145 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace SitecoreTactics.ThumbnailManager
+{
+    public class MediaConverter
+    {
+        #region Image Conversion
+        public static void ConvertPDFtoJPG(string pdfFile, int pageNo, string jpgFile)
+        {
+            GhostscriptSharp.GhostscriptWrapper.GeneratePageThumb(pdfFile, jpgFile, pageNo, 72, 72, false, false);
+        }
+
+        public static string GetImageOrientation(string jpgFile)
+        {
+            using (FileStream fs = new FileStream(jpgFile, FileMode.Open, FileAccess.ReadWrite))
+            {
+                using (System.Drawing.Image photo = System.Drawing.Image.FromStream(fs, true, false))
+                {
+                    if (photo.Width > photo.Height)
+                        return "L";
+                    else
+                        return "P";
+                }
+            }
+        }
+
+        public static void ReSizeJPG(string inputJpgFile, string outputJpgFile, int width, int height, bool isColor)
+        {
+            using (FileStream fs = new FileStream(inputJpgFile, FileMode.Open, FileAccess.ReadWrite))
+            {
+                using (System.Drawing.Image photo = System.Drawing.Image.FromStream(fs, true, false))
+                {
+                    using (System.Drawing.Image newImage = ScaleImage(photo, width, height, isColor))
+                    {
+                        newImage.Save(outputJpgFile, ImageFormat.Jpeg);
+                    }
+                }
+            }
+        }
+
+        private static System.Drawing.Image ScaleImage(System.Drawing.Image image, int maxWidth, int maxHeight, bool isColor)
+        {
+            if (maxWidth == 0 && maxHeight == 0)
+            {
+                maxWidth = image.Width;
+                maxHeight = image.Height;
+            }
+            else if (maxWidth == 0)
+            {
+                maxWidth = maxHeight * image.Width / image.Height;
+            }
+            else if (maxHeight == 0)
+            {
+                maxHeight = maxWidth * image.Height / image.Width;
+            }
+
+
+            var newImage = new Bitmap(maxWidth, maxHeight);
+
+            if (!isColor)
+            {
+                ColorMatrix colorMatrix = new ColorMatrix(
+                   new float[][] 
+                              {
+                                 new float[] {.3f, .3f, .3f, 0, 0},
+                                 new float[] {.59f, .59f, .59f, 0, 0},
+                                 new float[] {.11f, .11f, .11f, 0, 0},
+                                 new float[] {0, 0, 0, 1, 0},
+                                 new float[] {0, 0, 0, 0, 1}
+                              });
+
+                //create some image attributes
+                ImageAttributes attributes = new ImageAttributes();
+
+                //set the color matrix attribute
+                attributes.SetColorMatrix(colorMatrix);
+                Graphics.FromImage(newImage).DrawImage(image, new Rectangle(0, 0, maxWidth, maxHeight), 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, attributes);
+            }
+            else
+                Graphics.FromImage(newImage).DrawImage(image, 0, 0, maxWidth, maxHeight);
+
+            return newImage;
+        }
+
+        #endregion
+
+        #region Sitecore Media Conversion
+        public static void ConvertMediaItemToFile(MediaItem mediaItem, string fileName)
+        {
+            Assert.ArgumentNotNull(mediaItem, "mediaItem");
+
+            if (mediaItem.InnerItem["file path"].Length > 0)
+                return;
+
+            // Have to use "blob" field in order to workaround an issue with mediaItem.GetStream() 
+            // as it uses mediaItem.FileBased property that returns "true" if at least one version has a file based media asset.
+            var blobField = mediaItem.InnerItem.Fields["blob"];
+            Stream stream = blobField.GetBlobStream();
+            if (stream == null)
+            {
+                //Log.Warn(string.Format("Cannot find media data at item '{0}'", mediaItem.MediaPath), typeof(MediaStorageManager));
+                return;
+            }
+
+            //string fileName = GetFilename(mediaItem, stream);
+            string relativePath = Sitecore.IO.FileUtil.UnmapPath(fileName);
+            try
+            {
+                SaveToFile(stream, fileName);
+
+                stream.Flush();
+                stream.Close();
+
+            }
+            catch (Exception)
+            {
+                //Log.Error(string.Format("Cannot convert BLOB stream of '{0}' media item to '{1}' file", mediaItem.MediaPath, relativePath), ex, typeof(MediaStorageManager));
+            }
+        }
+
+        private static void SaveToFile(Stream stream, string fileName)
+        {
+            byte[] buffer = new byte[8192];
+            using (FileStream fs = File.Create(fileName))
+            {
+                int length;
+                do
+                {
+                    length = stream.Read(buffer, 0, buffer.Length);
+                    fs.Write(buffer, 0, length);
+                }
+                while (length > 0);
+
+                fs.Flush();
+                fs.Close();
+            }
+        }
+        #endregion
+    }
+}
---
+++ SitecoreTactics.ThumbnailManager/PDFThumbnailRequestHandler.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Web;
+using Sitecore.Configuration;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Events;
+using Sitecore.Resources.Media;
+using Sitecore.Web;
+using System.Web.SessionState;
+using System.IO;
+using Sitecore;
+
+namespace SitecoreTactics.ThumbnailManager
+{
+    class PDFThumbnailRequestHandler : Sitecore.Resources.Media.MediaRequestHandler, IRequiresSessionState
+    {
+        protected override bool DoProcessRequest(HttpContext context)
+        {
+
+            Assert.ArgumentNotNull(context, "context");
+
+            MediaRequest request = MediaManager.ParseMediaRequest(context.Request);
+            request.Options.UseMediaCache = true;
+
+            if (request == null)
+            {
+                return false;
+            }
+
+            Sitecore.Resources.Media.Media media = null;
+            try
+            {
+                media = MediaManager.GetMedia(request.MediaUri);
+
+                if (media != null)
+                    return this.DoProcessRequest(context, request, media);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Error while creating thumbnails of PDF, URL:" + context.Request.Url.ToString() + ". Exception:" + ex.ToString(), this);
+            }
+
+            if (media == null)
+            {
+                context.Response.Write("404 - File not found");
+                context.Response.End();
+            }
+            else
+            {
+                string itemNotFoundUrl = (Context.Site.LoginPage != string.Empty) ? Context.Site.LoginPage : Settings.NoAccessUrl;
+
+                if (Settings.RequestErrors.UseServerSideRedirect)
+                {
+                    HttpContext.Current.Server.Transfer(itemNotFoundUrl);
+                }
+                else
+                {
+                    HttpContext.Current.Response.Redirect(itemNotFoundUrl);
+                }
+            }
+
+            return true;
+        }
+
+
+
+        protected override bool DoProcessRequest(HttpContext context, MediaRequest request, Sitecore.Resources.Media.Media media)
+        {
+            Assert.ArgumentNotNull(context, "context");
+            Assert.ArgumentNotNull(request, "request");
+            Assert.ArgumentNotNull(media, "media");
+
+            if (this.Modified(context, media, request.Options) == Sitecore.Tristate.False)
+            {
+                Event.RaiseEvent("media:request", new object[] { request });
+                this.SendMediaHeaders(media, context);
+                context.Response.StatusCode = 0x130;
+                return true;
+            }
+
+            // Gets media stream for the requested media item thumbnail
+            MediaStream stream = ProcessThumbnail(request, media);
+            if (stream == null)
+            {
+                return false;
+            }
+            Event.RaiseEvent("media:request", new object[] { request });
+            this.SendMediaHeaders(media, context);
+            this.SendStreamHeaders(stream, context);
+            using (stream)
+            {
+                context.Response.AddHeader("Content-Length", stream.Stream.Length.ToString());
+                WebUtil.TransmitStream(stream.Stream, context.Response, Settings.Media.StreamBufferSize);
+            }
+            return true;
+        }
+
+        private MediaStream ProcessThumbnail(MediaRequest request, Sitecore.Resources.Media.Media media)
+        {
+            MediaStream mStream = null;
+
+            if (request.Options.Height == 0 && request.Options.Width == 0)
+            {
+                request.Options.Thumbnail = true;
+            }
+
+            mStream = MediaManager.Cache.GetStream(media, request.Options);
+
+            if (mStream == null)
+            {
+                string tempPath = Settings.TempFolderPath + "/PDF-Thumbnails/";
+
+                tempPath = MainUtil.MapPath(tempPath);
+
+                if (!Directory.Exists(tempPath))
+                {
+                    Directory.CreateDirectory(tempPath);
+                }
+
+                // Prepare filenames
+                string pdfFile = tempPath + media.MediaData.MediaItem.ID.ToString() + ".pdf";
+                string jpgFile = tempPath + media.MediaData.MediaItem.ID.ToString() + "_" + media.MediaData.MediaItem.InnerItem["revision"] + ".jpg";
+
+                string resizedJpgFile = tempPath + media.MediaData.MediaItem.ID.ToString() + "_" + request.Options.Width.ToString() + "_" + request.Options.Height.ToString();
+
+                if (!File.Exists(jpgFile))
+                {
+                    // Save BLOB media file to disk
+                    MediaConverter.ConvertMediaItemToFile(media.MediaData.MediaItem, pdfFile);
+                    
+                    // Convert PDF to Jpeg - First Pager
+                    MediaConverter.ConvertPDFtoJPG(pdfFile, 1, jpgFile);
+
+                }
+
+                // Resize Image
+                MediaConverter.ReSizeJPG(jpgFile, resizedJpgFile, request.Options.Width, request.Options.Height, true);
+
+                // Convert resized image to stream
+                mStream = new MediaStream(File.Open(resizedJpgFile, FileMode.Open, FileAccess.Read, FileShare.Read), "jpg", media.MediaData.MediaItem);
+                
+
+                // Add the requested thumbnail to Media Cache
+                MediaStream outStream = null;
+                MediaManager.Cache.AddStream(media, request.Options, mStream, out outStream);
+
+                if (outStream != null)
+                {
+                    // If Media cache is enabled
+                    return outStream;
+                }
+
+            }
+
+            // If Media cache is disabled
+            return mStream;
+        }
+    }
+}
